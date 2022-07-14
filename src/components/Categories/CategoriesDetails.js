48eab1ec4885445056411feecf0fe6fc5aefd6ae
Dasti Jaswanth Reddy <jaswanthreddydasti@gmail.com>
2022-07-15 02:31:53 +0530

    applied-aos

--- src/components/Categories/CategoriesDetails.js
+++ src/components/Categories/CategoriesDetails.js
@@ -1,9 +1,13 @@
 import React,{useEffect, useContext} from 'react';
 import { myContext } from '../context/context';
 import './CategoriesDetails.scss';
+import AOS from 'aos';
 const CategoriesDetails = () => {
   const {fetchCategoriesDetails,categoryDetails}=useContext(myContext);
   var location=window.location.pathname.substring(12);
+   AOS.init({
+    duration: 2000
+  });
   useEffect(()=>{
       fetchCategoriesDetails(location);
   },[fetchCategoriesDetails,location]);
@@ -11,7 +15,7 @@ const CategoriesDetails = () => {
     <div className='category-details'>
         {
             categoryDetails.map((meal)=>(
-                <div key={meal.idMeal}>
+                <div key={meal.idMeal} data-aos="zoom-in-up">
                     <img src={meal.strMealThumb} alt='#'/>
                     <h4>{meal.strMeal}</h4>
                 </div>
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,3 +1,4 @@
+import AOS from 'aos';
 import React, {useState,useCallback,useContext} from 'react';
 import { myContext } from '../context/context';
 import './HomePage.scss';
@@ -5,6 +6,9 @@ import './HomePage.scss';
 const HomePage = () => {
   const [searchTerm,setSearchTerm]=useState("");
   const {fetchHomePageMeals,meals}=useContext(myContext);
+  AOS.init({
+    duration: 2000
+  });
 
 const fetchMealsHandler=useCallback(()=>{
     fetchHomePageMeals(searchTerm);
@@ -27,11 +31,11 @@ const fetchMealsHandler=useCallback(()=>{
         </div>
         <div className='home-grid'>
             {meals ? meals.map((meal) => (
-              <div className='home-meal' key={meal.idMeal}>
+              <div className='home-meal' key={meal.idMeal} data-aos="zoom-in">
                 <a href={meal.strYoutube} target="_blank" rel="noopener noreferrer"><img src={meal.strMealThumb} alt='#' /></a>
                 <a href={meal.strSource} target="_blank" rel="noopener noreferrer"><h3>{meal.strMeal}</h3></a>
               </div>
-            )) : <div className='home-meal'>
+            )) : <div className='home-meal' data-aos="zoom-in">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7D1F6AgHuvYGUUA7JHV1gKP3ut89dfb83yA&usqp=CAU" alt='#' />
                 <h4>404</h4>
                 <h2>No meals found! Try another meal...</h2>
